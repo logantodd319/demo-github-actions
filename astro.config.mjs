b382bb32c23a7807f28f857957e37d819233f129
logantodd319 <137828154+logantodd319@users.noreply.github.com>
2025-05-08 15:08:47 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import prefetch from "@astrojs/prefetch";
 
 export default defineConfig({
   integrations: [tailwind(), sitemap(), prefetch()],
-  site: "https://adulbrich.github.io",
+  site: 'https://logantodd319.github.io/demo-github-actions/',
   base: "/demo-github-actions",
 });
